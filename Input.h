764011f18b6afc9dcadf1481fae8e88d7c8ba3ce
OnoMasachika <k021g1121@g.neec.ac.jp>
2022-10-24 14:04:20 +0900

    2_2 実行できない

---
+++ Input.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <windows.h>
+
+#include <wrl.h>
+
+#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
+#include <dinput.h>
+
+class Input {
+public:
+	template <class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
+public:
+	void Initialize(HINSTANCE hInstance, HWND hwnd);
+
+	void Update();
+
+private:
+	ComPtr<IDirectInputDevice8> keyboard;
+};
